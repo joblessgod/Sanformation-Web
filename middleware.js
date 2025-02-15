bb6cc12d1a9951ccd405c3dbbe26fa55d6e0240e
JobLessGod <iamjoblessgod@gmail.com>
2025-02-15 13:28:01 +0545

    added more routes for secure

--- middleware.js
+++ middleware.js
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/login(.*)'])
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/',
+    '/ncit'
+])
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
