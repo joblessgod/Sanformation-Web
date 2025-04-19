5ec871371039f2f8a8877031730318a8f9cccef1
JobLessGod <iamjoblessgod@gmail.com>
2025-04-19 13:52:20 +0545

    feat: updated metadata, added web banner

--- app/layout.js
+++ app/layout.js
@@ -13,21 +13,33 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Dummy Website",
-  description: "This is a dummy description for a dummy website.",
+  title: "Sanformation",
+  description:
+    "Explore the future of web development, design, and code with Sanformation.",
   openGraph: {
-    title: "Dummy Website",
-    description: "This is a description of the dummy website for Open Graph.",
-    url: "https://dummywebsite.com",
-    siteName: "Dummy Website",
+    title: "Sanformation",
+    description:
+      "Explore the future of web development, design, and code with Sanformation.",
+    url: "https://sanchit.info.np",
+    siteName: "Sanformation",
+    images: [
+      {
+        url: "https://sanchit.info.np/public/banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Sanformation Preview",
+      },
+    ],
     locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@dummyTwitterHandle",
-    title: "Dummy Website",
-    description: "This is a description of the dummy website for Twitter.",
+    site: "@Sanformation",
+    title: "Sanformation",
+    description:
+      "Explore the future of web development, design, and code with Sanformation.",
+    images: ["https://sanchit.info.np/public/banner.png"],
   },
 };
 
