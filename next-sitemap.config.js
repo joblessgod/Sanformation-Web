e2b78b0775652e82f72d73542d480455f799d046
JobLessGod <iamjoblessgod@gmail.com>
2025-04-19 19:01:58 +0545

    feat: add sitemap and robots.txt for SEO optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://sanchit.info.np",
+  generateRobotsTxt: true, // (optional)
+  // ...other options like exclude, changefreq, etc.
+};
