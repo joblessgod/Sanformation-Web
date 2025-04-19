064740ce4b7a8b81e0e384ece0f35e2e6e119b1d
JobLessGod <iamjoblessgod@gmail.com>
2025-04-19 19:01:47 +0545

    feat: add page component with metadata and improve Home component's Twitter card

--- app/layout.js
+++ app/layout.js
@@ -14,33 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Sanformation",
-  description:
-    "Explore the future of web development, design, and code with Sanformation.",
-  openGraph: {
-    title: "Sanformation",
-    description:
-      "Explore the future of web development, design, and code with Sanformation.",
-    url: "https://sanchit.info.np",
-    siteName: "Sanformation",
-    images: [
-      {
-        url: "https://sanchit.info.np/public/banner.png",
-        width: 1200,
-        height: 630,
-        alt: "Sanformation Preview",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@Sanformation",
-    title: "Sanformation",
-    description:
-      "Explore the future of web development, design, and code with Sanformation.",
-    images: ["https://sanchit.info.np/public/banner.png"],
-  },
+  description: "Sanchit Gives Information",
 };
 
 export default function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
+import Head from "next/head";
+
 export default function Home() {
   function handleLogin() {}
   return (
     <>
+      <Head>
+        <meta
+          name="twitter:card"
+          content="Explore the future of web development, design, and code with Sanformation."
+        />
+        <meta name="twitter:site" content="@Sanformation" />
+        <meta name="twitter:creator" content="@sanchit" />
+        <meta name="twitter:image" content="https://images.com/image.jpg" />
+      </Head>
       <main className="container mx-auto mt-8 w-full">Hello World</main>
     </>
   );
